3eb3a8bf5522a8d399d3732d655914291b161313
Gregor Richards <hg-yff@gregor.im>
2024-09-10 15:02:21 -0400

    Adding i386 teal support

--- ggt/native/ctx.h
+++ ggt/native/ctx.h
@@ -3,12 +3,27 @@
 
 #include "detect-ctx.h"
 
-#if GGGGT_SUPP_CTX_SWITCH == GGGGT_SUPP_CTX_SWITCH_X86_64
 static void ggtContextCreate(
     ggt_thread_t *thr, void *sp,
     void (*routine)(ggt_thread_t *, void *), void *arg
 ) {
     if (setjmp(thr->prev->ctx) == 0) {
+#if GGGGT_SUPP_CTX_SWITCH == GGGGT_SUPP_CTX_SWITCH_X86
+        __asm__ volatile(
+            "mov %2, %%esp\n"
+            "push %1\n"
+            "push %0\n"
+            "push $0\n"
+            "jmp *%3"
+            :
+            : "r" (thr),
+              "r" (arg),
+              "r" (sp),
+              "r" (routine)
+            : "rdi", "rsi"
+        );
+
+#elif GGGGT_SUPP_CTX_SWITCH == GGGGT_SUPP_CTX_SWITCH_X86_64
         __asm__ volatile(
             "movq %0, %%rdi\n"
             "movq %1, %%rsi\n"
@@ -21,8 +36,9 @@ static void ggtContextCreate(
               "r" (routine)
             : "rdi", "rsi"
         );
+
+#endif
     }
 }
-#endif
 
 #endif
